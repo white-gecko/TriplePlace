472a026503deadac33a3bf9c5794877a8dd94705
Natanael Arndt <arndtn@gmail.com>
2011-10-24 19:53:40 +0200

    run benchmark in thread

--- TriplePlace/src/org/aksw/tripleplace/ui/TriplePlaceActivity.java
+++ TriplePlace/src/org/aksw/tripleplace/ui/TriplePlaceActivity.java
@@ -11,7 +11,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.SlidingDrawer;
 import android.widget.TextView;
 
 public class TriplePlaceActivity extends Activity {
@@ -28,49 +27,68 @@ public class TriplePlaceActivity extends Activity {
 		Button go = (Button) this.findViewById(R.id.start);
 		go.setOnClickListener(new GoClick(text));
 	}
-	
+
 	private class GoClick implements OnClickListener {
-		
-		private TextView text; 
-		
-		public GoClick (TextView text) {
+
+		private TextView text;
+
+		public GoClick(TextView text) {
 			this.text = text;
 		}
-		
+
 		public void onClick(View v) {
-			long end = test4k();
-			text.setText("Status " + 1000*2*2 + " Tripel in " + end + " ms hinzugefügt, das sind " + (end/1000) + " s und " + (end/1000)/60 + " min done");
+			String path = getFilesDir().getAbsolutePath();
+			HexaBenchmark b = new HexaBenchmark(path, text);
+			b.start();
+			text.setText("läuft ...");
 		}
 	}
 
-	private long test4k() {
-		String path = getFilesDir().getAbsolutePath();
-		Hexastore hx = new Hexastore(path);
+	private class HexaBenchmark extends Thread {
 
-		Node s, p, o;
-		Triple triple;
+		private String path;
+		private TextView text;
+		private long end;
 
-		long start, end;
-		start = System.currentTimeMillis();
-		try {
-			for (int i = 0; i < 1000; i++) {
-				s = new Node("<http://" + i + ".eu>");
-				for (int j = 0; j < 2; j++) {
-					p = new Node("<http://xmlns.com/foaf/0.1/" + i + "," + j
-							+ ">");
-					for (int k = 0; k < 2; k++) {
-						o = new Node("\"Name" + i + "," + j + "," + k + "\"");
-						triple = new Triple(s, p, o);
-						hx.addTriple(triple);
+		public HexaBenchmark(String path, TextView text) {
+			this.text = text;
+			this.path = path;
+		}
+
+		public void run() {
+			String path = getFilesDir().getAbsolutePath();
+			Hexastore hx = new Hexastore(path);
+
+			Node s, p, o;
+			Triple triple;
+
+			long start, end;
+			start = System.currentTimeMillis();
+			try {
+				for (int i = 0; i < 1000; i++) {
+					s = new Node("<http://" + i + ".eu>");
+					for (int j = 0; j < 2; j++) {
+						p = new Node("<http://xmlns.com/foaf/0.1/" + i + ","
+								+ j + ">");
+						for (int k = 0; k < 2; k++) {
+							o = new Node("\"Name" + i + "," + j + "," + k
+									+ "\"");
+							triple = new Triple(s, p, o);
+							hx.addTriple(triple);
+						}
 					}
 				}
+			} catch (Exception e) {
+				Log.e(TAG, "Couldn't create new Node", e);
 			}
-		} catch (Exception e) {
-			Log.e(TAG, "Couldn't create new Node", e);
-		}
-		end = (System.currentTimeMillis() - start);
-		Log.v("Benchmark", "Status(" + end + ") done");
+			end = (System.currentTimeMillis() - start);
+			Log.v("Benchmark", "Status(" + end + ") done");
 
-		return end;
+			text.setText("Status " + 1000 * 2 * 2 + " Tripel in " + end
+					+ " ms hinzugefügt, das sind " + (end / 1000) + " s und "
+					+ (end / 1000) / 60 + " min done");
+			this.end = end;
+		}
 	}
+
 }
