934de15d27ed2e4bfcc4f478437140aaa6dccbfc
Natanael Arndt <arndtn@gmail.com>
2011-11-06 00:11:10 +0100

    Store throws Exception on export

--- TriplePlace/src/org/aksw/tripleplace/Store.java
+++ TriplePlace/src/org/aksw/tripleplace/Store.java
@@ -7,6 +7,6 @@ public interface Store {
 	public void addTriple(Triple triple) throws IOException;
 	public List<Triple> query(Triple triple) throws Exception;
 	public void removeTriple(Triple triple) throws IOException;
-	public List<Triple> export() throws IOException;
+	public List<Triple> export() throws Exception;
 	public Node getNode(String nodeString) throws Exception;
 }
